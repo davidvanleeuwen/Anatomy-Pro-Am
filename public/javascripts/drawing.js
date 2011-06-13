f508581fe9bb5bf92640ca674248acad07a0d0d6
Greg Vaughan <vaughan.greg@gmail.com>
2011-06-13 16:59:07 -0500

    dual display bug for score card resolved

--- public/javascripts/drawing.js
+++ public/javascripts/drawing.js
@@ -122,6 +122,9 @@ components.drawing = function(){
 			}.bind(this));
 			em.on('scoreEveryone', function (player){
 				if (player.current_case_id == me.get('current_case_id')){
+					this.score_card_template = _.template($('#score_card_template').html());
+					$("#score_popup_tag").html('');
+					$("#score_popup_tag").append(this.score_card_template());
 					$("#score_popup_tag").show();
 					this.done();
 				}
